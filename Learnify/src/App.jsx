3dc20bb23272bbda77b6df453ff47735d280c185
Deepak <Deepak@Deepaks-MacBook-Pro.local>
2025-06-10 10:25:39 +0530

    Clerk Setup done

--- Learnify/src/App.jsx
+++ Learnify/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import './App.css'
 import './Components/Navbar.jsx'
 import Navbar from './Components/Navbar.jsx'
 import Main from './Components/Main.jsx'
 import Top_Courses from './Components/Top-Courses.jsx'
+
 function App() {
 
   return (
@@ -14,6 +16,12 @@ function App() {
         <Navbar/>
         <Main/>
         <Top_Courses/>
+
+        <Router>
+          <Routes>
+            <Route index path="/" element={<Main/>}></Route>
+          </Routes>
+        </Router>
       </div>
       
     </>
--- Learnify/src/main.jsx
+++ Learnify/src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const Clerk_Key = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if(!Clerk_Key) throw new Error("Clerk Key Required")
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={Clerk_Key}>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
 )
